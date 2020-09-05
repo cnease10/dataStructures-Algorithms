572515241e333e279cbfc67e9509db89c8dfd018
cnease10 <cierra.nease10@gmail.com>
2020-09-05 12:54:29 -0700

    string reversal

---
+++ app.js
@@ -0,0 +1,10 @@
+//1. String Reversal
+//Given a string of characters as input, write a function that returns it with the characters reversed.
+const input = "string"
+function StringReversal() {
+    array = input.split("") 
+    array.reverse()
+    array.join()
+    console.log(array)
+}
+StringReversal();
