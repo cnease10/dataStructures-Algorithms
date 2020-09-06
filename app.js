3184e6f61553dcbee60735c59bf0ecc581816f61
cnease10 <cierra.nease10@gmail.com>
2020-09-05 21:56:49 -0700

    fizzbuzz

--- app.js
+++ app.js
@@ -41,3 +41,25 @@ function integerReverse() {
     console.log(intReversed)
 }
 integerReverse();
+
+//4. Fizz Buzz
+//Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 2, 
+//print out “Fizz”; when it’s divisible by 3, print out “Buzz”; when it’s divisible by both 2 and 3, print out “Fizz Buzz”.
+function fizzbuzz(n) {
+    for (let i = 0; i < n; i++) {
+        if (i % 2 == 0 && i % 3 == 0) {
+            console.log("fizzbuzz")
+        }
+        else if (i % 2 == 0) {
+            console.log("fizz")
+        }
+        else if (i % 3 == 0) {
+            console.log("buzz")
+        }
+        else {
+            console.log(i)
+        }
+
+    }
+}
+fizzbuzz(10);
