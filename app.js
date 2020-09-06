06e4e086478af3fdcd284e13a67308995819985f
cnease10 <cierra.nease10@gmail.com>
2020-09-05 21:42:41 -0700

    palindrome

--- app.js
+++ app.js
@@ -8,3 +8,22 @@ function StringReversal() {
     console.log(array)
 }
 StringReversal();
+
+//2. Palindrome
+//A palindrome is a word or phrase that reads the same backward as forward. Write a function that checks for this.
+const palindrome = "test"
+function CheckPalindrome() {
+    array = palindrome.split("")
+    palindromeReverse = array.reverse()
+    console.log(palindromeReverse)
+    palindromeReverseFinal = palindromeReverse.join("")
+    console.log(palindrome)
+    console.log(palindromeReverseFinal)
+    if (palindrome === palindromeReverseFinal) {
+        console.log(true)
+    } 
+    else {
+        console.log(false)
+        }
+}
+CheckPalindrome();
